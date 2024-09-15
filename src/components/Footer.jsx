99f0891645188ccb407734e68c0b39d583fbd817
Tevin Campbell <tevincampbell73@yahoo.com>
2024-09-15 10:02:57 -0400

    setup routes for navigation + footer finalied (for now)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="flex-center justify-center">
+      <p className="text-gray text-sm">
+        © Tevin Campbell 2024.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,33 @@
+import { Link } from "react-router-dom";
 import { navList } from "../constants/index";
-import logoImg from "/images/logoImg.svg"
+import logoImg from "/images/logoImg.svg";
 import PrimaryButton from "./ui/PrimaryButton";
 
 const Navbar = () => {
   return (
-    <>
-      <header className="full-width  pady-5 flex-center justify-between position-fixed">
-        <nav className="flex-center full-width">
-          <img src={logoImg} alt="Tevin's logo" width={20} height={20}
-          className="transition-all pointer"
+    <header className="full-width pady-5 flex-center justify-between position-fixed">
+      <nav className="flex-center full-width padx-5">
+        <Link to="/">
+          <img 
+            src={logoImg} 
+            alt="Tevin's logo" 
+            width={20} 
+            height={20} 
+            className="transition-all pointer"
           />
+        </Link>
 
-          <div className="flex justify-center text-gray pointer padx-5 text-sm list-gap transition-all">
-            {navList.map((nav) => (
-              <div key={nav} className="hover-white">{nav}</div>
-            ))}
-          </div>
+        <div className="flex justify-center text-gray pointer padx-5 text-sm list-gap transition-all">
+          {navList.map((nav) => (
+            <Link key={nav.name} to={nav.path} className="hover-white">
+              {nav.name}
+            </Link>
+          ))}
+        </div>
 
-          <PrimaryButton />
-        </nav>
-      </header>
-    </>
+        <PrimaryButton />
+      </nav>
+    </header>
   );
 };
 
