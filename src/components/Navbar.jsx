c0efec891e76cb747101e30621881c7a133be3c6
Tevin Campbell <tevincampbell73@yahoo.com>
2024-09-15 07:35:39 -0400

    organized svg code + navigation styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
+import { navList } from "../constants/index";
+import logoImg from "/images/logoImg.svg"
+import PrimaryButton from "./ui/PrimaryButton";
+
 const Navbar = () => {
-    return(
-        <>
-            <p>Nav</p>
-        </>
-    )
-}
+  return (
+    <>
+      <header className="full-width  pady-5 flex-center justify-between">
+        <nav className="flex-center full-width">
+          <img src={logoImg} alt="Tevin's logo" width={20} height={20}
+          className="transition-all pointer"
+          />
+
+          <div className="flex justify-center text-gray pointer padx-5 text-sm list-gap transition-all">
+            {navList.map((nav) => (
+              <div key={nav} className="hover-white">{nav}</div>
+            ))}
+          </div>
+
+          <PrimaryButton />
+        </nav>
+      </header>
+    </>
+  );
+};
 
 export default Navbar;
