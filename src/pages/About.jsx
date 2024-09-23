03a864256c5b3d8501ce0e466e68fcbf58ecc82f
Tevin Campbell <tevincampbell73@yahoo.com>
2024-09-23 13:39:01 -0400

    setup: for content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,13 @@
 const About = () => {
     return(
+        <div className="projects flex-center flex-column">
+        {/* make sure to change heading from h1 for symatic code and seo */}
         <h1>About me</h1>
+        
+        <div className="flex gap wrap justify-center">
+            <h2>Content</h2>
+        </div>
+    </div>
     )
 }
 
