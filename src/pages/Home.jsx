ebe0e1b198dde7f8bf01d986e2a1c14590885e07
Tevin Campbell <tevincampbell73@yahoo.com>
2024-10-08 14:58:27 -0400

    testing homepage layout spacing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,46 @@
 import CtaBtn from "../components/ui/CtaButton";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <section className="flex-center justify-center full-width full-height flex-column">
       <div className="flex-column flex-center">
-        <h1 className="heading-height hero-txt-efx">TEVIN CAMPBELL</h1>
-        <p id="byLine" className="text-sm text-gray">
-          Frontend Developer building what I love, one project at a time.
-        </p>
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            ease: "linear",
+            duration: 1,
+            delay: 0.2,
+          }}
+        >
+          <h1 className="heading-height hero-txt-efx">TEVIN CAMPBELL</h1>
+        </motion.h1>
+
+        <motion.p
+          initial={{ opacity: 0, y: -9 }}
+          animate={{ opacity: 1, y: 10 }}
+          transition={{ delay: 0.5, duration: 1, ease: "linear" }}
+        >
+          <p id="byLine" className="text-sm text-gray">
+            Frontend Developer building what I love, one project at a time.
+          </p>
+        </motion.p>
       </div>
 
       <div id="cta" className="flex-center reg-gap">
-        <div className="cto-outline flex-center justify-between">
-          <h3 id="cta-text" className="text-white">See Work</h3>
-        <CtaBtn />
-        </div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 2, duration: 1, ease: "linear" }}
+        >
+          <div className="cto-outline flex-center justify-between">
+            <h3 id="cta-text" className="text-white">
+              See Work
+            </h3>
+            <CtaBtn />
+          </div>
+        </motion.div>
       </div>
     </section>
   );
