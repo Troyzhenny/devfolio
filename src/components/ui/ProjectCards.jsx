753523de4e9299bfdc01aea44aac686cd284cfea
Troyzhenny <75921907+Troyzhenny@users.noreply.github.com>
2024-12-28 23:57:06 -0500

    fixed Project Cards + added link to live applications

--- src/components/ui/ProjectCards.jsx
+++ src/components/ui/ProjectCards.jsx
@@ -3,33 +3,34 @@ import PrimaryBtn from "./PrimaryBtn";
 import PropTypes from "prop-types";
 
 const ProjectCard = ({ project }) => {
-  const { title, description, codeUrl } = project;
-
   return (
-    // TODO: When card is hovered, top right corner an icon pops up to view live site
     <div id="card" className="card flex-center flex-column">
       <div id="deck" className="flex-center justify-center flex-column">
         <div className="flex-center justify-center flex-column">
-          <h2>{title}</h2>
-          <p className="text-white">{description}</p>
+          <h2>{project.title}</h2>
+          <p className="text-white">{project.description}</p>
         </div>
         <div className="flex-center pady-5 gap-sm">
-          <PrimaryBtn value="Code" url={codeUrl} />
+          <PrimaryBtn value="Code" url={project.codeUrl} />
           <br />
-          <LuArrowUpRight className="app--btn pointer" />
+          <LuArrowUpRight
+            className="app--btn pointer"
+            onClick={() => open(project.liveUrl)}
+          />
         </div>
       </div>
     </div>
   );
 };
 
-// prop-types validation
+
 ProjectCard.propTypes = {
   project: PropTypes.shape({
-    image: PropTypes.string.isRequired,
+    image: PropTypes.string,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     codeUrl: PropTypes.string,
+    liveUrl: PropTypes.string,
   }).isRequired,
 };
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -25,35 +25,35 @@ export const footerLinks = [
 
 export const projectList = [
   {
-    id: "1",
-    image: "/images/p1.jpg",
+    id:1,
     title: "Solarite",
     description:
       "Solarite is a web application designed to streamline solar panel services in Los Angeles.Built with Reactjs, Vite, Tailwind, and Supabase.",
     codeUrl: "https://github.com/chingu-voyages/v52-tier2-team-25/tree/main/solarite",
+    liveUrl: "https://v52-tier2-team-25.vercel.app/"
   },
   {
-    id: "2",
-    image: "/images/p1.jpg",
+    id: 2,
     title: "Me365.shop",
     description:
       "An engaging and user-friendly merch store built for YouTuber ME365. Features include dynamic product displays, secure checkout, and responsive design for an optimal shopping experience on any device.",
     codeUrl: "https://github.com/Troyzhenny/me365-shop",
+    liveUrl: ""
   },
-  // {
-  //   id: "3",
-  //   image: "/images/p1.jpg",
-  //   title: "Project 3",
-  //   description:
-  //     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit dolores culpa corrupti ea repudiandae.",
-  //   codeUrl: "https://github.com/Troyzhenny/playstation.com-clone",
-  // },
   {
-    id: "4",
-    image: "/images/p1.jpg",
+    id: 3,
+    title: "Windows 11",
+    description:
+      "A responsive recreation of the Windows 11 interface built with React and CSS, featuring interactive UI elements and a sleek design for a seamless user experience",
+    codeUrl: "https://github.com/Troyzhenny/win11ui",
+    liveUrl: ""
+  },
+  {
+    id: 4,
     title: "Portfolio v.1",
     description:
       "A modern, responsive portfolio website built to showcase my skills in web development and UI/UX design. Built from scratch using React, CSS, and Vite.",
     codeUrl: "https://github.com/Troyzhenny/devfolio",
+    liveUrl: "https://tevintc.xyz/"
   },
 ];
